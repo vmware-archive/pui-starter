a0b85def37e422df55b90adb60dc23ca5340e956
Jonathan Berney <jberney@pivotal.io>
2017-10-02 11:16:45 -0700

    add react-svg-loader as a dependency, and read env config correctly on the server side
    Signed-off-by: Stephane Jolicoeur <sjolicoeur@pivotal.io>

--- app/index.jsx
+++ app/index.jsx
@@ -2,9 +2,11 @@ import ReactDOMServer from 'react-dom/server';
 import Layout from './components/layout';
 import React from 'react';
 import Application from './components/application';
+import application from '../config/application.json';
 
 export default function Index(props, done) {
-  const {config = {}} = props;
+  const envConfig = require(`../config/${process.env.NODE_ENV}.json`);
+  const config = {...application, ...envConfig};
   const html = `<!DOCTYPE html>${ReactDOMServer.renderToStaticMarkup(<Layout {...{config}}><Application {...{config}}/></Layout>)}`;
   if (!done) return html;
   done(null, html);
