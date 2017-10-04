c64ac416a4101b7dac9a1d4d6c8d647065455f36
Ming Xiao <mxiao@pivotal.io>
2017-10-04 16:23:19 -0700

    Remove unused variable

--- spec/app/components/application_spec.js
+++ spec/app/components/application_spec.js
@@ -1,8 +1,6 @@
 require('../spec_helper');
 
 describe('Application', () => {
-  let TodoList;
-
   beforeEach(() => {
     const Application = require('../../../app/components/application');
     const config = {title: 'title'};
