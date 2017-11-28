4c55a40fb34a21f63c0844ca7cb8b67bfcd9986a
Jonathan Berney <jberney@pivotal.io>
2017-11-28 12:07:30 -0800

    pepper helper function
    Signed-off-by: Ming Xiao <mxiao@pivotal.io>

--- app/components/forms.js
+++ app/components/forms.js
@@ -3,7 +3,7 @@ import {Form, FormRow, FormCol} from 'pivotal-ui/react/forms';
 import {PrimaryButton} from 'pivotal-ui/react/buttons';
 import {Input} from 'pivotal-ui/react/inputs';
 import {Toggle} from 'pivotal-ui/react/toggle';
-import {pepper} from '../../helpers/application_helper';
+import {pepperOptions} from '../../helpers/application_helper';
 import {post} from '../../helpers/fetch_helper';
 
 export default class FormExample extends React.Component {
@@ -25,14 +25,9 @@ export default class FormExample extends React.Component {
         </FormRow>
         <FormRow>
           <FormCol inline optional name="spiciness" label="Spiciness"
-                   tooltip="There is only spicy!!" tooltipSize="sm" optionalText="">
+                   tooltip="There is only spicy!!" optionalText="">
             <select>
-              <option value="0"/>
-              <option value="1">{pepper}</option>
-              <option value="2">{pepper}{pepper}</option>
-              <option value="3">{pepper}{pepper}{pepper}</option>
-              <option value="4">{pepper}{pepper}{pepper}{pepper}</option>
-              <option value="5">{pepper}{pepper}{pepper}{pepper}{pepper}</option>
+              {pepperOptions}
             </select>
           </FormCol>
           <FormCol fixed inline name="glutenFree" label="Gluten Free" labelPosition="after" optional optionalText="">
--- helpers/application_helper.js
+++ helpers/application_helper.js
@@ -1,8 +1,20 @@
+import React from 'react';
+
+const pepper = `${String.fromCharCode(55356)}${String.fromCharCode(57142)}`;
+
 const helpers = {
   compact(array) {
     return array.filter(Boolean);
   },
-  pepper: `${String.fromCharCode(55356)}${String.fromCharCode(57142)}`
+  pepper,
+  pepperOptions: [
+    <option value="0"/>,
+    <option value="1">{pepper}</option>,
+    <option value="2">{pepper}{pepper}</option>,
+    <option value="3">{pepper}{pepper}{pepper}</option>,
+    <option value="4">{pepper}{pepper}{pepper}{pepper}</option>,
+    <option value="5">{pepper}{pepper}{pepper}{pepper}{pepper}</option>
+  ].map((e, key) => React.cloneElement(e, {key}))
 };
 
 module.exports = helpers;
