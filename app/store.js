2b6f037842c653315936081ea504a348223880ad
Jonathan Berney <jberney@pivotal.io>
2017-12-01 10:34:23 -0800

    data should be strings for consistency

--- app/store.js
+++ app/store.js
@@ -1,37 +1,37 @@
 module.exports = {
-  currentRoute: null,
+  currentRoute: '',
   todoItems: [],
   users: [],
   menu: [{
-    name: 'Food One', price: 12.50, glutenFree: false, seasonal: true, spiciness: 2, description: null
+    name: 'Food One', price: '12.50', glutenFree: false, seasonal: true, spiciness: 2, description: ''
   }, {
     name: 'Food Two',
-    price: 47.99,
+    price: '47.99',
     glutenFree: true,
     seasonal: true,
     spiciness: '',
     description: 'This is a description of food two.'
   }, {
     name: 'Food Three',
-    price: 100,
+    price: '100',
     glutenFree: false,
     seasonal: true,
-    spiciness: 5,
+    spiciness: '5',
     description: 'This is a description of food three. This is a description of food three. This is a description of food three. '
   }, {
-    name: 'Food Four', price: 7.00, glutenFree: false, seasonal: true, spiciness: 3, description: null
+    name: 'Food Four', price: '7.00', glutenFree: false, seasonal: true, spiciness: '3', description: ''
   }, {
-    name: 'Food Five', price: 88.19, glutenFree: true, seasonal: false, spiciness: 3, description: null
+    name: 'Food Five', price: '88.19', glutenFree: true, seasonal: false, spiciness: '3', description: ''
   }, {
     name: 'Food Six',
-    price: 123.45,
+    price: '123.45',
     glutenFree: false,
     seasonal: false,
-    spiciness: 4,
+    spiciness: '4',
     description: 'This is a description of food seven.'
   }, {
-    name: 'Food Seven', price: 55.55, glutenFree: true, seasonal: true, spiciness: 1, description: null
+    name: 'Food Seven', price: '55.55', glutenFree: true, seasonal: true, spiciness: '1', description: ''
   }, {
-    name: 'Food Eight', price: 415.99, glutenFree: true, seasonal: true, spiciness: 2, description: null
+    name: 'Food Eight', price: '415.99', glutenFree: true, seasonal: true, spiciness: '2', description: ''
   }]
 };
