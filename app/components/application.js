125419aa907ce5c40ae53a72f60199e700c46e2f
Reid Mitchell <rmitchell@pivotal.io>
2017-12-05 11:51:49 -0800

    use buttons for tabs to separate content
    Signed-off-by: Jonathan Berney <jberney@pivotal.io>

--- app/components/application.js
+++ app/components/application.js
@@ -5,6 +5,7 @@ import {Actions, useStore} from 'p-flux';
 import FormExample from './forms';
 import Menu from './menu';
 import MenuTable from './menu_table';
+import {DefaultButton} from 'pivotal-ui/react/buttons';
 import 'pivotal-ui/css/alignment';
 import 'pivotal-ui/css/box-shadows';
 import 'pivotal-ui/css/colors';
@@ -23,6 +24,11 @@ class Application extends React.Component {
     router: PropTypes.oneOfType([PropTypes.object, PropTypes.func])
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {tab: 0};
+  }
+
   componentDidMount() {
     const pathname = (window && window.location.pathname) || '/todoList';
     Actions.setRoute(pathname);
@@ -30,19 +36,31 @@ class Application extends React.Component {
 
   render() {
     const data = Actions.fetchMenu();
+    const {tab} = this.state;
+
     return (
       <div>
         <h1>Pivotal Thai Stop</h1>
-        <h2 className="h2">Menu</h2>
-        <div className="mhl mbxxl">
-          <Menu {...{data}}/>
+
+        <div className="mvxl txt-c">
+          <DefaultButton onClick={() => this.setState({tab: 0})} alt={tab !== 0}>Menu</DefaultButton>
+          <DefaultButton onClick={() => this.setState({tab: 1})} alt={tab !== 1}>Admin</DefaultButton>
         </div>
-        <hr/>
-        <h2 className="h2">Menu Admin Table</h2>
-        <MenuTable {...{data}}/>
-        <hr/>
-        <h2 className="h2">Add Menu Item</h2>
-        <FormExample/>
+
+        <div hidden={tab !== 0}>
+          <h2 className="h2">Menu</h2>
+          <div className="mhl mbxxl">
+            <Menu {...{data}}/>
+          </div>
+        </div>
+
+        <div hidden={tab !== 1}>
+          <h2 className="h2">Menu Admin Table</h2>
+          <MenuTable {...{data}}/>
+          <hr/>
+          <h2 className="h2">Add Menu Item</h2>
+          <FormExample/>
+        </div>
       </div>
     );
   }
